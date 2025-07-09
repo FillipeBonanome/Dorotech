5b18d62b978cc111fe0d36ce28eaf04bc42ad6df
FillipeBonanome <incarnialol2@gmail.com>
2025-07-09 01:17:01 -0300

    First Commit

---
+++ src/test/java/com/dorotech/Dorotech/DorotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorotech.Dorotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
